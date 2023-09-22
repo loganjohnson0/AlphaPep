534ea470c4b02da06d5b4531172b6c91c6363bbe
Logan Johnson <logan01@iastate.edu>
2023-09-22 13:51:18 -0500

    added visualization

--- R/get_alphafold2.R
+++ R/get_alphafold2.R
@@ -1,31 +1,38 @@
 #' This function gets an AlphaFold2 predicted structure
 #' @param uniprot_id A UniProt Accession Number for which the Alphafold predicted structure is desired
 #' 
-#'
 #' @importFrom httr GET
+#' @importFrom httr content
 #' @importFrom jsonlite fromJSON
 #' @importFrom utils download.file
+#' @importFrom magrittr %>% 
+#' @import NGLVieweR
 #' 
-#' @return A .pdb file from the AlphaFold2 Database
-#' @examples
-#' \dontrun{
-#' getalphafold2(uniprot_id = "P02540")
-#' }
 #' @export
 get_alphafold2 <- function(uniprot_id) {
   
-  url <- "https://alphafold.ebi.ac.uk/api/prediction/"
-  
-  url <- paste0(url, uniprot_id)
+  url <- paste0("https://alphafold.ebi.ac.uk/api/prediction/", uniprot_id)
   
   data <- httr::GET(url = url)
   
-  if (data$status_code != 200) {
-    print("There is not an AlphaFold predicted structure for this accession number. Please select a different protein.")
-  } else {
+  if (data$status_code == 200) {
     data <- jsonlite::fromJSON(httr::content(data, "text"))
     
     utils::download.file(url = data$pdbUrl,
                          destfile = paste0("./", "AlphaFold_Structure.pdb"))
+  } else {
+    print(paste0("There is not an AlphaFold predicted structure for UniProt Accession Number ", 
+                 uniprot_id, 
+                 ". Please select a different protein."))
   }
+  
+  NGLVieweR::NGLVieweR(data = "./AlphaFold_Structure.pdb") %>%
+    NGLVieweR::addRepresentation(type = "cartoon", 
+                                 param = list(
+                                   name = "cartoon", 
+                                   color = "residueindex")) %>%
+    NGLVieweR::stageParameters(backgroundColor = "black") %>%
+    NGLVieweR::setQuality("high") %>%
+    NGLVieweR::setFocus(0) %>%
+    NGLVieweR::setSpin(TRUE) 
 }
